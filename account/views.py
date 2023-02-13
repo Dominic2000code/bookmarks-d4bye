6068704d37561f582cb6202bf95e2987ba830d86
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-02-13 16:52:26 +0000

    added views for user and users page and a follow system

--- account/views.py
+++ account/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginFrom, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib.auth.models import User
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from .models import Contact
 
 
 # Create your views here.
@@ -75,3 +78,41 @@ def edit(request):
 
     context = {'user_form': user_form, 'profile_form': profile_form}
     return render(request, template_name, context)
+
+
+@login_required
+def user_list(request):
+    template_name = 'account/user/list.html'
+    users = User.objects.filter(is_active=True)
+    context = {'section': 'people', 'users': users}
+    return render(request, template_name, context)
+
+
+@login_required
+def user_detail(request, username):
+    template_name = 'account/user/detail.html'
+    user = get_object_or_404(User, username=username, is_active=True)
+    context = {'section': 'people', 'user': user}
+    return render(request, template_name, context)
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
